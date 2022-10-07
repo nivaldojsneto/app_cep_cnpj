1510ff3507f8d2394fb125b73d26cdcda127c459
Thiago Lopes <thiagoolopezz@gmail.com>
2022-10-07 20:15:50 -0300

    Integração Primeng

---
+++ src/app/primeng.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+@NgModule({
+  exports: [
+    ButtonModule,
+    InputTextModule
+  ]
+})
+export class PrimeNGModule {}
