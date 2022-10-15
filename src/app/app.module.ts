4089b3afcac2ea54803b80bf6edf31dd5b90699a
Thiago Lopes <thiagoolopezz@gmail.com>
2022-10-14 21:01:42 -0300

    Add PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { CnpjComponent } from './cnpj/cnpj.component';
 import { CepService } from './cep/cep.service';
 import { MessageService } from 'primeng/api';
 import { NgxSpinnerModule } from "ngx-spinner";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,13 @@ import { NgxSpinnerModule } from "ngx-spinner";
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    PrimeNGModule
+    PrimeNGModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [
